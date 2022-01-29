f09a272cfd8cd930889d4f19420ea9521734b2c7
Dwarfsobaka <dwarfsobaka@gmail.com>
2022-01-30 00:22:55 +0300

    2 files

--- src/main/java/com/example/currencyrate/service/Currency.java
+++ src/main/java/com/example/currencyrate/service/Currency.java
@@ -1,2 +1,13 @@
-package com.example.currencyrate.service;public class Currency {
+package com.example.currencyrate.service;
+
+public class Currency {
+    private String currency;
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
 }
